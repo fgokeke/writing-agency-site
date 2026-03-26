cde540c2674969abdcd7967221bb951b0fa3159c
okekefeargod <okekefeargod@gmail.com>
2026-03-26 14:16:11 +0100

    feat: Initialize Django project settings, including the 'website' app, static/media file configurations, and email backend.

--- agency/settings.py
+++ agency/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-&+z0ijn4i9=)s$1-9k^6^z9i!s4=t(8^fckf&n^whwooibou5!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.232.121.67']
 
 
 # Application definition
